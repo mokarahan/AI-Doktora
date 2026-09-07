08094853d0821fce7ae809dc36bd901bb6583cf5
mokarahan <ozgurkarahan@yahoo.com>
2026-09-07 13:40:18 +0300

    added

---
+++ prepare_data.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+OS_TYPE=$(uname -s)
+
+case "$OS_TYPE" in
+    Linux*)
+        echo "Running on Linux"
+        PROJECT_HOME=$HOME/Dev/doktora
+        source $HOME/.venv/bin/activate
+        python3 $PROJECT_HOME/src/Generator.py
+        ;;
+    Darwin*)
+        echo "Running on macOS"
+        PROJECT_HOME=$HOME/Dev/doktora
+        source $HOME/path/to/venv/bin/activate
+        python3 $PROJECT_HOME/src/Generator.py 
+        # Insert Mac-specific commands here
+        ;;
+    *)
+        echo "Unknown Operating System: $OS_TYPE"
+        ;;
+esac
+
+
